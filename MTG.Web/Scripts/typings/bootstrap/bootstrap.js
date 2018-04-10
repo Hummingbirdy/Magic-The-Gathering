25c1cfe9120c20e6854703c3781a6e81dec93390
tlemaster <lemaster.works@gmail.com>
2018-04-09 19:35:31 -0500

    add typings

---
+++ MTG.Web/Scripts/typings/bootstrap/bootstrap.js
@@ -0,0 +1,6 @@
+/// <reference path="../jquery/jquery.d.ts" />
+// Type definitions for Bootstrap 3.3.5
+// Project: http://twitter.github.com/bootstrap/
+// Definitions by: Boris Yankov <https://github.com/borisyankov/>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+//# sourceMappingURL=bootstrap.js.map
